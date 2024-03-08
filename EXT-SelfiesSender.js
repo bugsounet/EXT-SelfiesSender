fb2d65bcbdc41fb4b1b14525f02e55b383efb6f8
bugsounet <github@bugsounet.fr>
2024-03-08 01:44:00 +0100

    lint code

--- EXT-SelfiesSender.js
+++ EXT-SelfiesSender.js
@@ -1,8 +1,7 @@
-/**************************
-*  EXT-SelfiesSender v1.1 *
-*  Bugsounet              *
-*  03/2023                *
-***************************/
+/*********************
+*  EXT-SelfiesSender *
+*  Bugsounet         *
+*********************/
 
 Module.register("EXT-SelfiesSender", {
   defaults: {
@@ -14,7 +13,7 @@ Module.register("EXT-SelfiesSender", {
     sendMailAuto: false,
     sendMailConfig: {
       transport: {
-        host: 'smtp.mail.com',
+        host: "smtp.mail.com",
         port: 465,
         secure: true,
         auth: {
@@ -31,146 +30,146 @@ Module.register("EXT-SelfiesSender", {
     }
   },
 
-  start: function() {
-    this.IsShooting = false
-    this.lastPhoto = null
-    this.session = {}
-    this.ready = false
+  start () {
+    this.IsShooting = false;
+    this.lastPhoto = null;
+    this.session = {};
+    this.ready = false;
   },
 
-  getDom: function() {
-    var wrapper = document.createElement("div")
-    wrapper.style.display = 'none'
-    return wrapper
+  getDom () {
+    var wrapper = document.createElement("div");
+    wrapper.style.display = "none";
+    return wrapper;
   },
 
-  notificationReceived: function(noti, payload, sender) {
+  notificationReceived (noti, payload, sender) {
     switch(noti) {
       case "GA_READY":
         if (sender.name == "MMM-GoogleAssistant") {
-          this.sendSocketNotification("INIT", this.config)
-          this.ready= true
-          this.sendNotification("EXT_HELLO", this.name)
+          this.sendSocketNotification("INIT", this.config);
+          this.ready= true;
+          this.sendNotification("EXT_HELLO", this.name);
         }
-        break
+        break;
       case "EXT_SELFIES-RESULT":
-        if (!payload || !this.ready) return // prevent crash
-        this.lastPhoto = payload
-        this.sendSelfie(payload)
-        break
+        if (!payload || !this.ready) return; // prevent crash
+        this.lastPhoto = payload;
+        this.sendSelfie(payload);
+        break;
       case "EXT_SELFIES-CLEAN_STORE": // all is clean so reset all
-        if (!this.ready) return
-        this.lastPhoto = null
-        this.session = {}
-        break
+        if (!this.ready) return;
+        this.lastPhoto = null;
+        this.session = {};
+        break;
     }
   },
 
-  socketNotificationReceived: function(noti, payload) {
+  socketNotificationReceived (noti, payload) {
     switch(noti) {
       case "ERROR": // will display error with EXT-Alert
         this.sendNotification("EXT_ALERT", {
           type: "error",
           message: payload
-        })
+        });
     }
   },
 
- /** TelegramBot function **/
-  EXT_TELBOTCommands: function(commander) {
+  /** TelegramBot function **/
+  EXT_TELBOTCommands (commander) {
     commander.add({
-      command: 'selfie',
-      callback: 'cmdSelfie',
-      description: "Take a selfie.",
-    })
+      command: "selfie",
+      callback: "cmdSelfie",
+      description: "Take a selfie."
+    });
 
     commander.add({
-      command: 'emptyselfie',
-      callback: 'cmdEmptySelfie',
+      command: "emptyselfie",
+      callback: "cmdEmptySelfie",
       description: "Remove all selfie photos."
-    })
+    });
 
     commander.add({
-      command: 'lastselfie',
-      callback: 'cmdLastSelfie',
-      description: 'Display the last selfie shot taken.'
-    })
+      command: "lastselfie",
+      callback: "cmdLastSelfie",
+      description: "Display the last selfie shot taken."
+    });
   },
 
-  cmdSelfie: function(command, handler) {
-    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.")
-    let key = Date.now()
-    this.session[key] = handler
-    this.sendNotification("EXT_SELFIES-SHOOT", {TBkey:key})
+  cmdSelfie (command, handler) {
+    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.");
+    let key = Date.now();
+    this.session[key] = handler;
+    this.sendNotification("EXT_SELFIES-SHOOT", { TBkey:key });
   },
 
-  cmdLastSelfie: function(command, handler) {
-    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.")
+  cmdLastSelfie (command, handler) {
+    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.");
     if (this.lastPhoto) {
       if (handler.args) {
-        var args = handler.args.toLowerCase().split(" ")
+        var args = handler.args.toLowerCase().split(" ");
         switch (args[0]) {
           case "mail":
-            if (this.config.sendMail) this.sendSocketNotification("MAIL", this.lastPhoto.path)
-            handler.reply("TEXT", this.config.sendMail ? "Selfie sended by mail.": "Command disabled.")
-            break
+            if (this.config.sendMail) this.sendSocketNotification("MAIL", this.lastPhoto.path);
+            handler.reply("TEXT", this.config.sendMail ? "Selfie sended by mail.": "Command disabled.");
+            break;
           case "screen":
-            this.sendNotification("EXT_SELFIES-LAST")
-            handler.reply("TEXT", "This is last selfie!")
-            break
+            this.sendNotification("EXT_SELFIES-LAST");
+            handler.reply("TEXT", "This is last selfie!");
+            break;
           case "gphotos":
-            if (this.config.sendGooglePhotos) this.sendNotification("EXT_GPHOTOPHOTOS-UPLOAD", this.lastPhoto.path)
-            handler.reply("TEXT", this.config.sendGooglePhotos ? "Selfie sended to EXT-GooglePhotos for upload.": "Command disabled.")
-            break
+            if (this.config.sendGooglePhotos) this.sendNotification("EXT_GPHOTOPHOTOS-UPLOAD", this.lastPhoto.path);
+            handler.reply("TEXT", this.config.sendGooglePhotos ? "Selfie sended to EXT-GooglePhotos for upload.": "Command disabled.");
+            break;
           default:
-            handler.reply("TEXT", 'Need Help for /lastselfie commands ?\n\n\
+            handler.reply("TEXT", "Need Help for /lastselfie commands ?\n\n\
   *mail*: will send last photo by mail\n\
   *screen*: will display last photo in the screen of MagicMirror²\n\
   *gphotos*: will upload last selfie in google photo directory (need EXT-GooglePhotos)\n\
   With no argument: will send last photo in private message\
-  ',{parse_mode:'Markdown'})
-            break
+  ",{ parse_mode:"Markdown" });
+            break;
         }
       } else {
-        handler.reply("PHOTO_PATH", this.lastPhoto.path)
+        handler.reply("PHOTO_PATH", this.lastPhoto.path);
       }
     } else {
-      handler.reply("TEXT", "Couldn't find the last selfie.")
+      handler.reply("TEXT", "Couldn't find the last selfie.");
     }
   },
 
-  cmdEmptySelfie: function(command, handler) {
-    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.")
-    this.sendNotification("EXT_SELFIES-EMPTY_STORE")
-    this.lastPhoto = null
-    this.session = {}
-    handler.reply("TEXT", "done.")
+  cmdEmptySelfie (command, handler) {
+    if (this.IsShooting) return handler.reply("TEXT", "Not available actually.");
+    this.sendNotification("EXT_SELFIES-EMPTY_STORE");
+    this.lastPhoto = null;
+    this.session = {};
+    handler.reply("TEXT", "done.");
   },
 
-  sendSelfie: function(result) {
+  sendSelfie (result) {
     // send to the user with TBkey
-    let sendTBAdmin = true
+    let sendTBAdmin = true;
     if (result.options.TBkey && this.session[result.options.TBkey]) {
-      sendTBAdmin= false
-      var handler = this.session[result.options.TBkey]
-      handler.reply("PHOTO_PATH", result.path)
-      this.session[result.options.TBkey] = null
-      delete this.session[result.options.TBkey]
+      sendTBAdmin= false;
+      var handler = this.session[result.options.TBkey];
+      handler.reply("PHOTO_PATH", result.path);
+      this.session[result.options.TBkey] = null;
+      delete this.session[result.options.TBkey];
     }
 
-    if (result.options.useTBKeyOnly) return // cas d'utilisation de sauvegarde locale uniquement (ignore le reste)
+    if (result.options.useTBKeyOnly) return; // cas d'utilisation de sauvegarde locale uniquement (ignore le reste)
 
-    if (this.config.sendGooglePhotos && this.config.sendGooglePhotosAuto) this.sendNotification("EXT_GPHOTOPHOTOS-UPLOAD", result.path)
+    if (this.config.sendGooglePhotos && this.config.sendGooglePhotosAuto) this.sendNotification("EXT_GPHOTOPHOTOS-UPLOAD", result.path);
 
     // send to admins
     if (this.config.sendTelegramBotAuto && sendTBAdmin) {
-      this.sendNotification("EXT_TELBOT-TELL_ADMIN", "New Selfie")
+      this.sendNotification("EXT_TELBOT-TELL_ADMIN", "New Selfie");
       this.sendNotification("EXT_TELBOT-TELL_ADMIN", {
         type: "PHOTO_PATH",
         path: result.path
-      })
+      });
     }
 
-    if (this.config.sendMail && this.config.sendMailAuto) this.sendSocketNotification("MAIL", result.path)
+    if (this.config.sendMail && this.config.sendMailAuto) this.sendSocketNotification("MAIL", result.path);
   }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,7 @@
-/**************************
-*  EXT-SelfiesSender v1.1 *
-*  Bugsounet              *
-*  03/2023                *
-***************************/
+/*********************
+*  EXT-SelfiesSender *
+*  Bugsounet         *
+**********************/
 
 var log = () => { /* do nothing */ };
 
@@ -10,63 +9,62 @@ var NodeHelper = require("node_helper");
 var nodemailer = require("nodemailer");
 
 module.exports = NodeHelper.create({
-  start: function() {
-    this.transporter = null
-    this.mailerIsReady = false
+  start () {
+    this.transporter = null;
+    this.mailerIsReady = false;
   },
 
-  initialize: function(payload) {
-    console.log("[SELFIES-SENDER] EXT-SelfiesSender Version:", require('./package.json').version, "rev:", require('./package.json').rev)
-    this.config = payload
-    if (payload.debug) log = (...args) => { console.log("[SELFIES-SENDER]", ...args) }
-    if (this.config.sendMail) this.TestMailConfig()
+  initialize (payload) {
+    console.log("[SELFIES-SENDER] EXT-SelfiesSender Version:", require("./package.json").version, "rev:", require("./package.json").rev);
+    this.config = payload;
+    if (payload.debug) log = (...args) => { console.log("[SELFIES-SENDER]", ...args); };
+    if (this.config.sendMail) this.TestMailConfig();
   },
 
-  socketNotificationReceived: function(noti, payload) {
+  socketNotificationReceived (noti, payload) {
     switch (noti) {
       case "INIT":
-        this.initialize(payload)
-        break
+        this.initialize(payload);
+        break;
       case "MAIL":
-        this.sendMail(payload)
-        break
+        this.sendMail(payload);
+        break;
     }
   },
 
-  sendMail: function(file) {
-    if (!this.mailerIsReady) return console.error("[SELFIES-SENDER] sendMailConfig is not ready for send selfies by mail!")
+  sendMail (file) {
+    if (!this.mailerIsReady) return console.error("[SELFIES-SENDER] sendMailConfig is not ready for send selfies by mail!");
     try {
-      var msg = Object.assign({}, this.config.sendMailConfig.message, {attachments: [{path: file}]})
-      this.transporter.sendMail(msg, err => {
+      var msg = Object.assign({}, this.config.sendMailConfig.message, { attachments: [{ path: file }] });
+      this.transporter.sendMail(msg, (err) => {
         if (err) {
-          console.error("[SELFIES-SENDER] Error, Failed to send mail." , err)
-          this.sendSocketNotification("ERROR", "Failed to send selfies by mail!")
-          return
+          console.error("[SELFIES-SENDER] Error, Failed to send mail." , err);
+          this.sendSocketNotification("ERROR", "Failed to send selfies by mail!");
+          return;
         }
-        log("Email sent successfully! [" + file + "]")
-      })
+        log(`Email sent successfully! [${file}]`);
+      });
     } catch (e) {
-      console.error("[SELFIES-SENDER] Error, Invalid mail account configuration.", e)
-      this.sendSocketNotification("ERROR", "Invalid mail account configuration.")
-      return
+      console.error("[SELFIES-SENDER] Error, Invalid mail account configuration.", e);
+      this.sendSocketNotification("ERROR", "Invalid mail account configuration.");
     }
   },
 
-  TestMailConfig: function() {
+  TestMailConfig () {
     if (!typeof this.config.sendMailConfig == "object") {
-      this.sendSocketNotification("ERROR", "SendMailConfig is not configured!")
-      return console.error("[SELFIES-SENDER] sendMailConfig is not configured!")
+      this.sendSocketNotification("ERROR", "SendMailConfig is not configured!");
+      return console.error("[SELFIES-SENDER] sendMailConfig is not configured!");
     }
-    this.transporter = nodemailer.createTransport(this.config.sendMailConfig.transport)
-    this.transporter.verify(error => {
+    this.transporter = nodemailer.createTransport(this.config.sendMailConfig.transport);
+    this.transporter.verify((error) => {
       if (error) {
-        console.error("[SELFIES-SENDER] sendMailConfig", error)
-        this.mailerIsReady = false
-        this.sendSocketNotification("ERROR", "SendMailConfig check failed, check backlog for details")
+        console.error("[SELFIES-SENDER] sendMailConfig", error);
+        this.mailerIsReady = false;
+        this.sendSocketNotification("ERROR", "SendMailConfig check failed, check backlog for details");
       } else {
-        console.log("[SELFIES-SENDER] Yes! We are able to send your selfies by mail.")
-        this.mailerIsReady = true
+        console.log("[SELFIES-SENDER] Yes! We are able to send your selfies by mail.");
+        this.mailerIsReady = true;
       }
-    })
+    });
   }
 });
